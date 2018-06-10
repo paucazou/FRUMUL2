2035bd7d3eaa6e05324630de0a5eab9a85fa41bc
paucazou <paucazou@yahoo.fr>
2018-06-10 22:50:02 +0200

    Added a getID in lexer and improved line/column track

--- frumul/lexer.cpp
+++ frumul/lexer.cpp
@@ -1,17 +1,25 @@
 #include "lexer.h"
 #include <iostream>
+#include <locale>
+
 
 namespace frumul {
 #ifdef DEBUG
 	void Lexer::test () {
 	bst::str s{"#ifNdef"};
 		std::cout << "Recognized: " << (recognizeCaselessID(s)? "Yes":"No") << "\n";
+		Token tok{getID()};
+		std::cout << tok.getValue() << "\n";
+		std::cout << tok;
+		std::cout << pos << "\n";
+		std::cout << current_char << '-' << raw_current_char;
+
 
 	}
 #endif
 
-	Lexer::Lexer (bst::str& nsource):
-		source{nsource}
+	Lexer::Lexer (const bst::str& nsource, const bst::str& nfilepath):
+		source{nsource}, filepath{nfilepath}, current_char{nsource.uAt(0)}, raw_current_char{nsource.uRawAt(0)}
 	{}
 
 	template <typename ...T>
@@ -38,31 +46,68 @@ namespace frumul {
 	void Lexer::advanceTo (int npos) {
 		/* Change the position and the current char
 		 * by advancing to npos
+		 * Change column and line. 
 		 */
-		pos = npos;
-		if (pos < source.uLength())
-			current_char = source.uAt(pos);
-		else // end of input
+		if (npos < source.uLength()) {
+			current_char = source.uAt(npos);
+			raw_current_char = source.uRawAt(npos);
+			// columns and lines
+			bst::str range {source.uRange(pos,npos)};
+			int lnumber = range.lineNumber();
+			if (lnumber > 1) { // TODO à vérifier
+				for (int i{npos}; range.uAt(i) != "\n"; --i)
+
+				column = 1 + npos - i; 
+				line += lnumber-1;
+			}
+			else
+				column = column + npos -pos;
+		}
+		else {// end of input 
 			current_char = "";
+			raw_current_char = 0;
+			line = source.lineNumber();
+			column = source.getLine(line).uLength();
+		}
+		pos = npos;
 	}
 
-	bool Lexer::recognizeCaselessID (bst::str& candidate) {
+	bool Lexer::recognizeCaselessID (const bst::str& candidate) {
 		/* True if candidate can be found in source
 		 * at pos position.
+		 * candidate must be an ASCII string.
 		 */
 		tempos = pos;
+		bst::str c, d;
 		for (int cpos{0};
 				cpos < candidate.uLength() && 
 				tempos < source.uLength();
 				++cpos, ++tempos
 		    ) {
-			bst::str c{source.uAt(tempos)};
+			c = source.uAt(tempos);
 			c.tolower();
-			bst::str d{candidate.uAt(cpos)};
+			d = candidate.uAt(cpos);
 			d.tolower();
 			if (c != d)
 				return false;
 		}
 		return true;
+	}
+	Token Lexer::getID() { // maybe return the value only TODO
+		/* Recognize an ID
+		 * and return the token.
+		 * An ID starts and ends with whitespace.
+		 */
+		//std::setlocale(LC_ALL,std::locale("").name().data()); WARNING do not forget to set locale
+		static bst::str unbreakable_space{L'\u00A0'}; // unbreakable space isn't considered a space
+		bst::str value;
+		Point start{column,line};
+		while (!std::iswspace(raw_current_char) && current_char != unbreakable_space && current_char != "") {
+			value += current_char;
+			advanceBy();
+		}
+		Point end{column-1,start.getLine()};
+		return Token{Token::ID,value,start,end,filepath,source};
+
 	}
 }// namespace
--- frumul/lexer.h
+++ frumul/lexer.h
@@ -9,21 +9,27 @@ namespace frumul {
 		/* Tokenize the document
 		 */
 		public:
-			Lexer (bst::str& nsource);
+			Lexer (const bst::str& nsource, const bst::str& nfilepath);
 			template <typename ...T>
 			Token getNextToken(T ...expected);
 			void test();
 		private:
 			//attributes
-			bst::str& source;
+			const bst::str& source;
+			const bst::str& filepath;
 			int pos{0}; // used to get the current position in the source
 			int tempos{0}; // used to get temporary look further
+			int line{1}; // current line and column
+			int column{1};
+
 			bst::str current_char; 
+			cpUcs4 raw_current_char;
 			//functions
 			Token _getNextToken(std::initializer_list<Token::Type> expected);
 			void advanceBy (int step=1); 
 			void advanceTo (int npos);
-			bool recognizeCaselessID (bst::str& candidate);
+			bool recognizeCaselessID (const bst::str& candidate);
+			Token getID ();
 	};
 }//namespace
 
