e4871dd6766886ee8e0cf039a3847a5bfea1cff7
paucazou <paucazou@yahoo.fr>
2018-06-20 15:57:38 +0200

    Changed Position private interface

--- frumul/position.cpp
+++ frumul/position.cpp
@@ -1,16 +1,58 @@
 #include "position.h"
 #include <string>
 namespace frumul {
-	Position::Position (int nstart, int nend, const bst::str& fp, const bst::str& fc) : start{getLineColumn(nstart,fc)}, end{getLineColumn(nend,fc)}, filepath{fp}, filecontent{fc}
+	
+	// constructors
+
+	Position::Position (int c1, int l1, int c2, int l2, const bst::str& fp, const bst::str& fc) :
+		start{getPosFromPoint(c1,l1,fc)}, end{getPosFromPoint(c2,l2,fc)}, filepath{fp}, filecontent{fc}
 	{
 	}
+
+	Position::Position (int c1, int l1, int c2, int l2, const char*  fp, const bst::str& fc) :
+		start{getPosFromPoint(c1,l1,fc)}, end{getPosFromPoint(c2,l2,fc)}, filepath{fp}, filecontent{fc}
+	{
+	}
+
+	Position::Position (const Point& p1, const Point& p2, const bst::str& fp, const bst::str& fc) :
+		start{getPosFromPoint(p1,fc)}, end{getPosFromPoint(p2,fc)}, filepath{fp}, filecontent{fc}
+	{
+	}
+
+	Position::Position (const Point& p1, const Point& p2, const char* fp, const bst::str& fc) :
+		start{getPosFromPoint(p1,fc)}, end{getPosFromPoint(p2,fc)}, filepath{fp}, filecontent{fc}
+	{
+	}
+
+	Position::Position (int nstart, int nend, const bst::str& fp, const bst::str& fc) : start{nstart}, end{nend}, filepath{fp}, filecontent{fc}
+	{
+	}
+
+	// getters
+	
+	Point Position::getStartPoint() const {
+		return getLineColumn(start,filecontent);
+	}
+
+	Point Position::getEndPoint() const {
+		return getLineColumn(end,filecontent);
+	}
+
+	//other functions
+	
 	bst::str Position::toString () const {
+		/* Return a string representation
+		 * of the instance
+		 */
+		Point startp {getLineColumn(start,filecontent)};
+		Point endp {getLineColumn(end,filecontent)};
+
 		bst::str returned{"File: "};
 		returned += filepath + '\n';
-		returned += bst::str{" "} * (start.getColumn() + bst::str{start.getLine()}.uLength() ) + "↓\n";
-		for (int cline{start.getLine()}; cline <= end.getLine();++cline)
+		returned += bst::str{" "} * (startp.getColumn() + bst::str{startp.getLine()}.uLength() ) + "↓\n";
+		for (int cline{startp.getLine()}; cline <= endp.getLine();++cline)
 			returned += bst::str(cline) + " " + filecontent.getLine(cline) + '\n';
-		returned += bst::str{" "} * (end.getColumn() + bst::str{end.getLine()}.uLength() ) + "↑\n";
+		returned += bst::str{" "} * (endp.getColumn() + bst::str{endp.getLine()}.uLength() ) + "↑\n";
 
 		bst::str temp_line;
 		int max_length{0};
@@ -22,10 +64,6 @@ namespace frumul {
 		bst::str sep{bst::str("=")*max_length + '\n'};
 		returned = sep + returned + sep;
 		return returned;
-	}
-	std::ostream& operator<<(std::ostream& out, const Position& pos) {
-		out << pos.toString();
-		return out;
 	}
 
 	Point Position::getLineColumn (int pos, const bst::str& string) {
@@ -42,5 +80,34 @@ namespace frumul {
 			}
 		column = pos -lastline;
 		return Point(column,line);
+	}
+
+	int Position::getPosFromPoint (const Point& p, const bst::str& string) {
+		/* Get the position inside string
+		 * from Point
+		 */
+		int pos{0};
+		int lcount{1}; 
+		for (auto it{string.begin()}; it != string.end(); ++it, ++pos) {
+			if (*it == '\n')
+				++lcount;
+			if (lcount == p.getLine())
+				break;
+		}
+		return pos -1 + p.getColumn(); // -1: because column starts at 1, not 0
+	}
+
+	int Position::getPosFromPoint (const int c, const int l, const bst::str& string) {
+		/* Get the position inside string
+		 * from c (column) and l (line)
+		 */
+		return getPosFromPoint(Point{c,l},string);
+	}
+
+	// overload
+	
+	std::ostream& operator<<(std::ostream& out, const Position& pos) {
+		out << pos.toString();
+		return out;
 	}
 }//namespace
--- frumul/position.h
+++ frumul/position.h
@@ -32,44 +32,41 @@ class Position {
 	 * nodes, parts of symbols, etc.
 	 */
 	private:
-		const Point start;	// first char
-		const Point end; 	// last char
+		const int start;	// first char
+		const int end; 		// last char
 		const bst::str filepath;
 		const bst::str& filecontent;
 	public:
 		//constructors
 		Position (int c1, int l1, int c2, int l2,
-			      const bst::str& fp, const bst::str& fc) :
-			start{c1,l1}, end{c2,l2}, filepath{fp}, filecontent{fc}
-		{}
+			      const bst::str& fp, const bst::str& fc);
 		Position (int c1, int l1, int c2, int l2,
-			       const char *fp, const bst::str& fc) :
-			start{c1,l1}, end{c2,l2}, filepath{fp}, filecontent{fc}
-		{}
-		Position (const Point& p1, const Point& p2, const bst::str& fp, const bst::str& fc):
-			start{p1}, end{p2}, filepath{fp}, filecontent{fc}
-		{}
-		Position (const Point& p1, const Point& p2, const char *fp, const bst::str& fc):
-			start{p1}, end{p2}, filepath{fp}, filecontent{fc}
-		{}
+			       const char *fp, const bst::str& fc);
+		Position (const Point& p1, const Point& p2, const bst::str& fp, const bst::str& fc);
+		Position (const Point& p1, const Point& p2, const char *fp, const bst::str& fc);
 		Position (int nstart, int nend, const bst::str& fp, const bst::str& fc); // nstart and nend are index in fc
 		Position (const Position& pos) :
 			start{pos.start}, end{pos.end}, filepath{pos.filepath}, filecontent{pos.filecontent}
 		{}
 
 		//getters
-		const Point& getStart()const {
+		int getStart()const {
 			return start;
 		}
-		const Point& getEnd() const{
+		int getEnd() const{
 			return end;
 		}
+		Point getStartPoint() const;
+		Point getEndPoint() const;
+
 		const bst::str & getFilePath () const {
 			return filepath;
 		}
 		// other functions
 		bst::str toString () const;
 		static Point getLineColumn (int pos, const bst::str& string);
+		static int getPosFromPoint (const Point& p, const bst::str& string);
+		static int getPosFromPoint (const int c, const int l, const bst::str& string);
 		// overload
 		friend std::ostream& operator<<(std::ostream& out, const Position& pos);
 };
