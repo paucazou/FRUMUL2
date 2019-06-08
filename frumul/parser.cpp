79ead9cd2ed6eeaebc9b33a033155e605aaa6992
paucazou <paucazou@yahoo.fr>
2019-06-08 23:26:54 +0200

    Added tests

--- frumul/parser.cpp
+++ frumul/parser.cpp
@@ -78,7 +78,7 @@ namespace frumul {
 		 * delete current_token and set a new one
 		 * return false if type does not match.
 		 */
-#if DEBUG && 1
+#if DEBUG && 0
 		std::cout << *current_token << std::endl;
 #endif
 		if (current_token->getType() == t) {
